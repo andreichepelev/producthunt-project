27c67df989af5dae2e6ced7d1477b4feb421e19e
Pirunpoika <kirkkonen@gmail.com>
2019-07-24 19:45:44 +0300

    Homepage added, site polished

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create', views.create, name='create'),
+    path('<int:product_id>', views.details, name='details'),
+    path('<int:product_id>/upvote', views.upvote, name='upvote'),
 ]
